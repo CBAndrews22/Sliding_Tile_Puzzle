c377d9bf8b8ae717b04dbcc9e383dee5cbfc1083
CBAndrews22 <cb.andrews92@gmail.com>
2022-10-29 14:04:24 -0700

    Adding user interface

--- Project_1.cpp
+++ Project_1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <ctime>
+#include <string>
 #include "Puzzle_Game.h"
 #include "Node.h"
 #include "Uniform_Cost_Search.h"
@@ -14,30 +15,49 @@ Puzzle* generalSearch(Puzzle*, MisplacedTile);
 Puzzle* generalSearch(Puzzle*, ManhattenDist);
 
 int main(){
-    // std::cout << "Initial Board \n" ;
-    std::vector<std::vector<int>> depth;
-    depth.push_back({1,2,3,4,5,6,7,8,0}); // depth 0
-    depth.push_back({1,2,3,4,5,6,0,7,8}); // depth 2
-    depth.push_back({1,2,3,5,0,6,4,7,8}); // depth 4
-    depth.push_back({1,3,6,5,0,2,4,7,8}); // depth 8
-    depth.push_back({1,3,6,5,0,7,4,8,2}); // depth 12
-    depth.push_back({1,6,7,5,0,3,4,8,2}); // depth 16
-    std::cout << "Let the games begin!!\nWhat would you like to do?\n";
 
-    Puzzle* UCSanswer; 
-    Puzzle* MTanswer;
-    Puzzle* MDanswer;
-    UniformCost UCS;
-    MisplacedTile MT; 
-    ManhattenDist MD;
+    std::cout << "Welcome to The Eight-Puzzle Solver.\nChoose an option.\n\t(1) Use Defalut Puzzle\n\t(2) Enter custom puzzle\n\t(3) Quit\n";
+    int userOption;
+    std::cin >> userOption;
 
-
-    for(int i=0; i < depth.size(); i++){
-        Puzzle* StartState = new Puzzle(depth[i]);
-        UCSanswer = generalSearch(StartState, UCS);
-        MTanswer = generalSearch(StartState, MT);
-        MDanswer = generalSearch(StartState, MD);
+    std::vector<int> initPuzzle;
+    if(userOption == 1){
+        initPuzzle = {1,3,6,5,0,7,4,8,2};
     }
+    else if(userOption == 2){
+        std::cout << "Enter custom eight-puzzle where 0 represents the blank tile:\nexample: 123406478\n";
+        std::string userPuzzle;
+        std::cin >> userPuzzle;
+        for(int i=0; i < userPuzzle.size(); i++){
+            initPuzzle.push_back((int)(userPuzzle[i]));
+        }
+    }
+
+    
+
+
+    
+    // std::vector<std::vector<int>> depth;
+    // depth.push_back({1,2,3,4,5,6,7,8,0}); // depth 0
+    // depth.push_back({1,2,3,4,5,6,0,7,8}); // depth 2
+    // depth.push_back({1,2,3,5,0,6,4,7,8}); // depth 4
+    // depth.push_back({1,3,6,5,0,2,4,7,8}); // depth 8
+    // depth.push_back({1,3,6,5,0,7,4,8,2}); // depth 12
+    // depth.push_back({1,6,7,5,0,3,4,8,2}); // depth 16
+
+    // Puzzle* UCSanswer; 
+    Puzzle* MTanswer;
+    // Puzzle* MDanswer;
+    // UniformCost UCS;
+    MisplacedTile MT; 
+    // ManhattenDist MD;
+
+    // for(int i=0; i < depth.size(); i++){
+    Puzzle* StartState = new Puzzle(initPuzzle);
+    //     UCSanswer = generalSearch(StartState, UCS);
+    MTanswer = generalSearch(StartState, MT);
+    //     MDanswer = generalSearch(StartState, MD);
+    // }
     
     return 0;
 }
